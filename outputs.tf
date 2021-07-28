b22bcdb8ac6bd49335bb247234a4d51a1de55adb
Andrew Trice <amtrice@us.ibm.com>
2021-07-28 13:04:15 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "name" {
   description = "The name of the catalog that was installed"
   value       = local.catalog_name
   depends_on  = [
-    helm_release.ibm_operator_catalog,
+    null_resource.ibm_operator_catalog,
     null_resource.create_entitlement_secret,
     null_resource.setup_global_pull_secret
   ]
